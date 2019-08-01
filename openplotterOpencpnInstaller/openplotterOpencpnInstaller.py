f536d1795ebcce7e1e5bac974127dba2eb488702
Sailoog <info@sailoog.com>
2019-08-01 12:23:15 +0200

    first commit

---
+++ openplotterOpencpnInstaller/openplotterOpencpnInstaller.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python3
+
+# This file is part of Openplotter.
+# Copyright (C) 2015 by sailoog <https://github.com/sailoog/openplotter>
+#
+# Openplotter is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
+# Openplotter is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Openplotter. If not, see <http://www.gnu.org/licenses/>.
+
+import wx, os, webbrowser, subprocess
+import wx.richtext as rt
+
+from openplotterSettings import conf
+from openplotterSettings import language
+
+class MyFrame(wx.Frame):
+	def __init__(self):
+		self.conf = conf.Conf()
+		self.currentdir = os.path.dirname(__file__)
+		currentLanguage = self.conf.get('GENERAL', 'lang')
+		self.language = language.Language(self.currentdir,'openplotter-opencpn-installer',currentLanguage)
+
+		wx.Frame.__init__(self, None, title='OpenCPN Installer', size=(800,444))
+		self.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+		icon = wx.Icon(self.currentdir+"/data/opencpn.png", wx.BITMAP_TYPE_PNG)
+		self.SetIcon(icon)
+
+		self.toolbar1 = wx.ToolBar(self, style=wx.TB_TEXT)
+		toolHelp = self.toolbar1.AddTool(101, _('Help'), wx.Bitmap(self.currentdir+"/data/help.png"))
+		self.Bind(wx.EVT_TOOL, self.OnToolHelp, toolHelp)
+		self.toolbar1.AddSeparator()
+		toolTranslate = self.toolbar1.AddTool(103, _('Translate'), wx.Bitmap(self.currentdir+"/data/crowdin.png"))
+		self.Bind(wx.EVT_TOOL, self.OnToolTranslate, toolTranslate)
+		self.toolbar1.AddSeparator()
+		toolUpdate = self.toolbar1.AddTool(104, _('Update Packages Data'), wx.Bitmap(self.currentdir+"/data/package.png"))
+		self.Bind(wx.EVT_TOOL, self.OnToolUpdate, toolUpdate)
+		
+		self.notebook = wx.Notebook(self)
+		self.apps = wx.Panel(self.notebook)
+		self.output = wx.Panel(self.notebook)
+		self.notebook.AddPage(self.apps, _('OpenCPN packages'))
+		self.notebook.AddPage(self.output, _('Output'))
+		self.il = wx.ImageList(24, 24)
+		img0 = self.il.Add(wx.Bitmap(self.currentdir+"/data/opencpn24.png", wx.BITMAP_TYPE_PNG))
+		img1 = self.il.Add(wx.Bitmap(self.currentdir+"/data/output.png", wx.BITMAP_TYPE_PNG))
+		self.notebook.AssignImageList(self.il)
+		self.notebook.SetPageImage(0, img0)
+		self.notebook.SetPageImage(1, img1)
+
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		vbox.Add(self.toolbar1, 0, wx.EXPAND)
+		vbox.Add(self.notebook, 1, wx.EXPAND)
+		self.SetSizer(vbox)
+
+		self.CreateStatusBar()
+		font_statusBar = self.GetStatusBar().GetFont()
+		font_statusBar.SetWeight(wx.BOLD)
+		self.GetStatusBar().SetFont(font_statusBar)
+		self.Centre(True) 
+		self.Show(True)
+
+		self.pageApps()
+		self.onListAppsDeselected()
+		self.pageOutput()
+
+	def ShowStatusBar(self, w_msg, colour):
+		self.GetStatusBar().SetForegroundColour(colour)
+		self.SetStatusText(w_msg)
+
+	def ShowStatusBarRED(self, w_msg):
+		self.ShowStatusBar(w_msg, wx.RED)
+
+	def ShowStatusBarGREEN(self, w_msg):
+		self.ShowStatusBar(w_msg, wx.GREEN)
+
+	def ShowStatusBarBLACK(self, w_msg):
+		self.ShowStatusBar(w_msg, wx.BLACK) 
+
+	def ShowStatusBarYELLOW(self, w_msg):
+		self.ShowStatusBar(w_msg,(255,140,0)) 
+
+	def OnToolHelp(self, event): 
+		url = "/usr/share/openplotter-doc/xxx/xxx.html"
+		webbrowser.open(url, new=2)
+
+	def OnToolTranslate(self, event): 
+		url = "https://crowdin.com/project/opencpn"
+		webbrowser.open(url, new=2)
+
+	def OnToolUpdate(self, event):
+		self.logger.Clear()
+		command = 'sudo apt update'
+		popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+		for line in popen.stdout:
+			self.logger.WriteText(line)
+			self.ShowStatusBarYELLOW(_('Updating packages data, please wait... ')+line)
+		self.notebook.ChangeSelection(1)
+		self.logger.ShowPosition(self.logger.GetLastPosition())
+		self.ShowStatusBarGREEN(_('Done. Now you can check if there are available updates'))
+		self.readApps()
+
+	def pageApps(self):
+		self.listApps = wx.ListCtrl(self.apps, -1, style=wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_HRULES, size=(-1,200))
+		self.listApps.InsertColumn(0, _('Name'), width=210)
+		self.listApps.InsertColumn(1, _('Category'), width=130)
+		self.listApps.InsertColumn(2, _('Installed'), width=170)
+		self.listApps.InsertColumn(3, _('Candidate'), width=170)
+		self.listApps.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onListAppsSelected)
+		self.listApps.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.onListAppsDeselected)
+
+		self.toolbar2 = wx.ToolBar(self.apps, style=wx.TB_TEXT | wx.TB_VERTICAL)
+		self.installButton = self.toolbar2.AddTool(201, _('Install'), wx.Bitmap(self.currentdir+"/data/add.png"))
+		self.Bind(wx.EVT_TOOL, self.OnInstallButton, self.installButton)
+		self.uninstallButton = self.toolbar2.AddTool(202, _('Uninstall'), wx.Bitmap(self.currentdir+"/data/remove.png"))
+		self.Bind(wx.EVT_TOOL, self.OnUninstallButton, self.uninstallButton)
+		self.toolbar2.AddSeparator()
+		self.openButton = self.toolbar2.AddTool(203, _('Info'), wx.Bitmap(self.currentdir+"/data/open.png"))
+		self.Bind(wx.EVT_TOOL, self.OnOpenButton, self.openButton)
+		self.changelogButton = self.toolbar2.AddTool(204, _('Changelog'), wx.Bitmap(self.currentdir+"/data/text.png"))
+		self.Bind(wx.EVT_TOOL, self.OnChangelogButtonButton, self.changelogButton)
+
+		sizer = wx.BoxSizer(wx.HORIZONTAL)
+		sizer.Add(self.listApps, 1, wx.EXPAND, 0)
+		sizer.Add(self.toolbar2, 0)
+		self.apps.SetSizer(sizer)
+
+		self.readApps()
+
+	def pageOutput(self):
+		self.logger = rt.RichTextCtrl(self.output, style=wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_DONTWRAP|wx.LC_SORT_ASCENDING)
+		self.logger.SetMargins((10,10))
+
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		sizer.Add(self.logger, 1, wx.EXPAND, 0)
+		self.output.SetSizer(sizer)
+
+	def OnInstallButton(self,e):
+		index = self.listApps.GetFirstSelected()
+		if index == -1: return
+		apps = list(reversed(self.apps))
+		package = apps[index]['package']
+		msg = _('Are you sure you want to install ')+package+_(' and its dependencies?')
+		dlg = wx.MessageDialog(None, msg, _('Question'), wx.YES_NO | wx.NO_DEFAULT | wx.ICON_EXCLAMATION)
+		if dlg.ShowModal() == wx.ID_YES:
+			self.logger.Clear()
+			command = 'sudo apt -y install '+package
+			popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+			for line in popen.stdout:
+				self.logger.WriteText(line)
+				self.ShowStatusBarYELLOW(_('Installing package, please wait... ')+line)
+			self.notebook.ChangeSelection(1)
+			postInstallation = apps[index]['postInstallation']
+			if postInstallation:
+				popen = subprocess.Popen(postInstallation, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+				for line in popen.stdout:
+					self.logger.WriteText(line)
+					self.ShowStatusBarYELLOW(_('Running post-installation scripts, please wait... ')+line)
+			self.logger.ShowPosition(self.logger.GetLastPosition())
+			self.ShowStatusBarGREEN(_('Done'))
+			dlg.Destroy()
+			self.readApps()
+		else: dlg.Destroy()
+
+	def OnUninstallButton(self,e):
+		index = self.listApps.GetFirstSelected()
+		if index == -1: return
+		apps = list(reversed(self.apps))
+		package = apps[index]['package']
+		msg = _('Are you sure you want to uninstall ')+package+_(' and its dependencies?')
+		dlg = wx.MessageDialog(None, msg, _('Question'), wx.YES_NO | wx.NO_DEFAULT | wx.ICON_EXCLAMATION)
+		if dlg.ShowModal() == wx.ID_YES:
+			self.logger.Clear()
+			command = 'sudo apt -y autoremove '+package
+			popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+			for line in popen.stdout:
+				self.logger.WriteText(line)
+				self.ShowStatusBarYELLOW(_('Uninstalling packages, please wait... ')+line)
+			self.notebook.ChangeSelection(1)
+			self.logger.ShowPosition(self.logger.GetLastPosition())
+			self.ShowStatusBarGREEN(_('Done'))
+			dlg.Destroy()
+			self.readApps()
+		else: dlg.Destroy()
+
+	def OnChangelogButtonButton(self,e):
+		index = self.listApps.GetFirstSelected()
+		if index == -1: return
+		apps = list(reversed(self.apps))
+		self.logger.Clear()
+		command = 'apt changelog '+apps[index]['package']
+		popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+		for line in popen.stdout:
+			self.logger.WriteText(line)
+			self.ShowStatusBarYELLOW(_('Reading changelog, please wait... ')+line)
+		self.notebook.ChangeSelection(1)
+		self.ShowStatusBarGREEN(_('Done'))
+
+	def OnOpenButton(self,e):
+		index = self.listApps.GetFirstSelected()
+		if index == -1: return
+		apps = list(reversed(self.apps))
+		entryPoint = apps[index]['entryPoint']
+		popen = subprocess.Popen(entryPoint, shell=True)
+
+	def readApps(self):
+		self.listApps.DeleteAllItems()
+		self.apps = []
+
+		app = {
+		'name': _('Weather Routing Plugin'),
+		'category': _('Weather'),
+		'package': 'opencpn-plugin-weatherrouting',
+		'sources': ['http://ppa.launchpad.net/opencpn/opencpn/ubuntu'],
+		'dev': 'no',
+		'entryPoint': 'x-www-browser https://opencpn.org/OpenCPN/plugins/weatherroute.html',
+		'postInstallation': 'opencpnPostInstallation',
+		}
+		self.apps.append(app)
+
+		app = {
+		'name': _('S-63 Charts Plugin'),
+		'category': _('Chart Support'),
+		'package': 'opencpn-plugin-s63',
+		'sources': ['http://ppa.launchpad.net/opencpn/opencpn/ubuntu'],
+		'dev': 'no',
+		'entryPoint': 'x-www-browser https://opencpn.org/OpenCPN/plugins/s63.html',
+		'postInstallation': 'opencpnPostInstallation',
+		}
+		self.apps.append(app)
+
+		app = {
+		'name': _('oeSENC Charts Plugin'),
+		'category': _('Chart Support'),
+		'package': 'oesenc-pi',
+		'sources': ['http://ppa.launchpad.net/opencpn/opencpn/ubuntu'],
+		'dev': 'no',
+		'entryPoint': 'x-www-browser https://opencpn.org/OpenCPN/plugins/oesenc.html',
+		'postInstallation': 'opencpnPostInstallation',
+		}
+		self.apps.append(app)
+
+		app = {
+		'name': _('OpenCPN'),
+		'category': '',
+		'package': 'opencpn',
+		'sources': ['http://ppa.launchpad.net/opencpn/opencpn/ubuntu'],
+		'dev': 'no',
+		'entryPoint': 'x-www-browser https://opencpn.org/',
+		'postInstallation': 'opencpnPostInstallation',
+		}
+		self.apps.append(app)
+
+		for i in self.apps:
+			item = self.listApps.InsertItem(0, i['name'])
+			self.listApps.SetItem(item, 1, i['category'])
+
+			installed = ''
+			candidate = ''
+			command = 'LANG=C apt-cache policy '+i['package']
+			popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+			for line in popen.stdout:
+				if 'Installed:' in line: installed = line
+				if 'Candidate:' in line: candidate = line
+			if installed:
+				installedL = installed.split(':')
+				installed = installedL[1]
+			if candidate:
+				candidateL = candidate.split(':')
+				candidate = candidateL[1]
+			if '(none)' in installed: installed = ''
+
+			missing = False
+			for ii in i['sources']:
+				command = 'apt-cache policy'
+				popen = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+				exists = False
+				for line in popen.stdout:
+					if ii in line: exists = True
+				if not exists: missing = ii
+			if missing: candidate = _('missing source: ')+missing
+
+			if i['dev'] == 'yes': 
+				candidate = _('coming soon')
+				self.listApps.SetItemBackgroundColour(item,(200,200,200))
+
+			self.listApps.SetItem(item, 2, installed)
+			self.listApps.SetItem(item, 3, candidate)
+
+	def onListAppsSelected(self, e):
+		i = e.GetIndex()
+		valid = e and i >= 0
+		if not valid: return
+		self.onListAppsDeselected()
+		self.toolbar2.EnableTool(203,True)
+		if self.listApps.GetItemBackgroundColour(i) != (200,200,200):
+			self.toolbar2.EnableTool(201,True)
+			self.toolbar2.EnableTool(202,True)
+			if self.listApps.GetItemText(i, 2) != '':
+				self.toolbar2.EnableTool(204,True)
+
+	def onListAppsDeselected(self, event=0):
+		self.toolbar2.EnableTool(201,False)
+		self.toolbar2.EnableTool(202,False)
+		self.toolbar2.EnableTool(203,False)
+		self.toolbar2.EnableTool(204,False)
+
+def main():
+	app = wx.App()
+	MyFrame().Show()
+	app.MainLoop()
+
+if __name__ == '__main__':
+	main()
