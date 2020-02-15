07cfc9b2a3b8dc314e7633bc898bcce2fcae28fa
Sailoog <info@sailoog.com>
2020-02-15 13:53:11 +0100

    add version to window name

--- openplotterOpencpnInstaller/openplotterOpencpnInstaller.py
+++ openplotterOpencpnInstaller/openplotterOpencpnInstaller.py
@@ -21,6 +21,7 @@ import wx.richtext as rt
 from openplotterSettings import conf
 from openplotterSettings import language
 from openplotterSettings import platform
+from .version import version
 
 class MyFrame(wx.Frame):
 	def __init__(self):
@@ -439,7 +440,7 @@ class MyFrame(wx.Frame):
 		}
 		self.appsDict.append(app)
 
-		wx.Frame.__init__(self, None, title=_('OpenCPN Installer'), size=(800,444))
+		wx.Frame.__init__(self, None, title=_('OpenCPN Installer')+' '+version, size=(800,444))
 		self.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
 		icon = wx.Icon(self.currentdir+"/data/openplotter-opencpn-installer.png", wx.BITMAP_TYPE_PNG)
 		self.SetIcon(icon)
